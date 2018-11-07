5835138b9651d4016398f9913e7f9121043e01a8
deedeeh <dinahafez.8@gmail.com>
2018-11-07 12:00:33 +0000

    Done with fetch toys and create a new toy

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const toysURL = 'http://localhost:3000'
+
+
+const getToys = () =>
+  fetch(`${toysURL}/toys`)
+  .then(resp => resp.json())
+
+
+const getToy = id =>
+  fetch(`${toysURL}/toys/${id}`)
+  .then(resp => resp.json())
+
+
+const createToy = toy =>
+  fetch(`${toysURL}/toys`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(toy)
+  }).then(resp => resp.json())
+
+// const updateToy = toy =>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+const toyNameInput = document.querySelector('#name-input')
+const toyImageInput = document.querySelector('#image-input')
+const submitForm = document.getElementById('toy-submit')
 let addToy = false
 
 // YOUR CODE HERE
+
+const state = {
+  toys: []
+}
+
+const renderToy = toy => {
+  const toyItem = document.createElement('div')
+  toyItem.className = 'card'
+  toyItem.innerHTML =
+  `<h2>${toy.name}</h2>
+  <img src='${toy.image}' class='toy-avatar' />
+  <p>${toy.likes}</p>
+  <button class='like-btn'>Like &#9829;</button>`
+  toyCollection.appendChild(toyItem)
+}
+
+const renderToys = toys => {
+  toys.forEach(toy => renderToy(toy))
+}
+
+getToys()
+  .then(toys => {
+    state.toys = toys
+    renderToys(toys)
+  })
+
+const newToyEvenetListener = () => {
+    toyForm.addEventListener('submit', event => {
+    console.log(event.target)
+    event.preventDefault();
+
+    const toy = {
+      name: toyNameInput.value,
+      image: toyImageInput.value,
+      likes: 0
+    }
+
+    createToy(toy)
+      .then(toy => renderToy(toy))
+    event.target.reset();
+  })
+}
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -10,7 +57,8 @@ addBtn.addEventListener('click', () => {
   if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
-  } else {
+    newToyEvenetListener()
+    } else {
     toyForm.style.display = 'none'
   }
 })
