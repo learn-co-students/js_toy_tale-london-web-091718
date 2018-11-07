595f8c06a53578579d52765e194850469880071e
Ed Putans <edputans@gmail.com>
2018-11-07 10:45:36 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyList = document.querySelector('#toy-collection')
 let addToy = false
 
 // YOUR CODE HERE
+
+// OR HERE!
+const retrieveToys = () => fetch('http://localhost:3000/toys').then(result => result.json())
+
+
+
+const likeToy = toyObject => {
+  toyObject.likes ++
+  fetch(`http://localhost:3000/toys/${toyObject.id}`, {
+  method: "PATCH",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify(toyObject)
+  })
+}
+
+
+const showToy = toyObject => {
+const toyElement = document.createElement("div")
+toyElement.innerHTML = `
+<div class = 'card'>
+<h2 data-id="${toyObject.id}">${toyObject.name}</h2>
+<img src="${toyObject.image}" class="toy-avatar">
+<p id="like-number">Likes: ${toyObject.likes}</p>
+<button class="like-btn">Like the plastic out of me!</button>
+</div>`
+
+
+const likeButton = toyElement.querySelector('.like-btn')
+likeButton.addEventListener('click', event => { 
+  likeToy(toyObject)
+  const likeNum=toyElement.querySelector('#like-number')
+  likeNum.innerText = `Likes: ${toyObject.likes}`
+})
+
+toyList.appendChild(toyElement)
+}
+
+
+const showToys = toys => toys.forEach(toy => showToy(toy))
+
+const retrieveToysToPage = () => retrieveToys().then(toys=>showToys(toys))
+
+
+const addNewToyRequest = toy => {
+  fetch('http://localhost:3000/toys',{
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(toy)
+  })
+ }
+
+
+ //upon page load:
+
+retrieveToysToPage()
+
+// Event listeners:
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +74,10 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
-// OR HERE!
+toyForm.addEventListener('submit', event => {
+  const toyName = document.querySelector('#nameruni').value
+  const toyImage = document.querySelector('#imageruni').value
+  let toyObject = {name: toyName, image: toyImage, likes: 0}
+  addNewToyRequest(toyObject).then(
+  retrieveToysToPage())
+})
