a3a9d9b5a1cbb25abaed7be6e02faf1157c9451f
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-07 14:38:01 +0000

    working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyDisplay = document.querySelector('#toy-collection')
+const toyFormInput = toyForm.querySelector('.add-toy-form')
 let addToy = false
+let localToys
 
 // YOUR CODE HERE
 
@@ -16,3 +19,67 @@ addBtn.addEventListener('click', () => {
 })
 
 // OR HERE!
+
+// Function to display a single toy
+const displayToy = toy => {
+  const toyItem = document.createElement('div')
+  toyItem.className = 'card'
+  toyItem.id = `${toy.id}`
+  toyItem.innerHTML = `
+  <h2>${toy.name}</h2>
+  <img src=${toy.image} class="toy-avatar" />
+  <p>${toy.likes}</p>
+  <button class="like-btn">Like <3</button>
+  `
+
+  toyDisplay.appendChild(toyItem)
+}
+
+// Function to display all toys
+const displayToys = toys => toys.forEach(toy => displayToy(toy))
+
+// Fetch and display all toys
+const renderToys = () => {
+  getToys()
+    .then(toys => {
+      localToys = toys
+      toyDisplay.innerHTML = ``
+      displayToys(toys)
+    })
+}
+
+renderToys()
+
+// Listen to new toy form submission, create toy
+toyForm.addEventListener('submit', event => {
+  event.preventDefault()
+  let inputValues = toyFormInput.querySelectorAll('.input-text')
+  const toy = {
+    name: inputValues[0].value,
+    image: inputValues[1].value,
+    likes: 0
+  }
+
+  createToys(toy)
+    .then(toy => displayToy(toy))
+    .then()
+
+  getToys()
+    .then(toys => {
+      localToys = toys
+      inputValues[0].value = ``
+      inputValues[1].value = ``
+    })
+})
+
+document.addEventListener('click', event => {
+  if (event.target.className === 'like-btn') {
+    let targetBtn = event.target
+    let toyId = parseInt(targetBtn.parentElement.id)
+    let toy = localToys.find(toy => toy.id === toyId)
+    toy.likes++
+    toyDisplay.innerHTML = ``
+    updateToy(toy)
+    displayToys(localToys)
+  }
+})
