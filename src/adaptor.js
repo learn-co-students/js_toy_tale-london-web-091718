aaccb7576c4c1607608ba3a38f92ff2f8cf06625
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-07 15:15:14 +0000

    completed all parts of practice but render toy after adding not working properly

---
+++ src/adaptor.js
@@ -0,0 +1,24 @@
+const baseURL = 'http://localhost:3000/toys'
+
+const getToys = () => 
+    fetch(baseURL)
+    .then(resp => resp.json())
+
+const addNewToy = toy =>
+    fetch(baseURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(toy)
+    })
+
+const updateToy = toy =>
+    fetch(baseURL + `/${toy.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(toy)
+    })
+    
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const submitButton = document.querySelector('.submit')
+const toyCollection = document.querySelector('#toy-collection')
+const toyNameInput = document.querySelector('#name-input')
+const toyImageInput = document.querySelector('#image-input')
+const likeButton = document.querySelector('.like-btn')
 let addToy = false
 
-// YOUR CODE HERE
+let state = {
+  toys: []
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +22,61 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+// event listener to whole doc looking for clicks on like buttons and then triggering update functions
+document.addEventListener('click', event => {
+  if (event.target.className === 'like-btn') {
+    const id = event.target.dataset.id // dataset.id relates to the data-id tag
+    const foundToy = state.toys.find(toy => toy.id === parseInt(id)) // need to parse id because it will be a string from html
+    const likeText = document.querySelector(`p[data-id='${id}']`)
+    const likedToy = updateToyLikes(foundToy)
+    likeText.innerText = `${likedToy.likes} Likes`
+    updateToy(foundToy)
+  }
+})
 
-// OR HERE!
+// increment number of likes
+const updateToyLikes = toy => {
+  ++toy.likes
+  return toy
+}
+
+// render a toy to page 
+const renderToy = toy => {
+  const toyEl = document.createElement('span')
+  toyEl.innerHTML = `
+  <div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p data-id=${toy.id}>${toy.likes} Likes </p>
+    <button data-id=${toy.id} class="like-btn">Like <3</button>
+  </div>
+  `
+  toyCollection.appendChild(toyEl)
+}
+
+
+// create new toy button will 1) create new toy 2) add to api 3) render to page
+submitButton.addEventListener('click', event => {
+    event.preventDefault()
+  const toy = {
+    name: toyNameInput.value,
+    image: toyImageInput.value,
+    likes: 0
+  }
+
+  addNewToy(toy)
+    .then(toy => renderToy(toy))
+
+  toyNameInput.value = ''
+  toyImageInput.value = ''
+})
+
+
+
+// fetching data from api and passing the response (array of toys) into renderToy function
+getToys()
+  .then(toys => {
+    state.toys = toys
+    state.toys.forEach(toy => renderToy(toy))
+  })
+
